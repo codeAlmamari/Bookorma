8a307c737697af98853a3ddc9b35f7b12963af37
Abdulrahman Almamari <codemamari@gmail.com>
2024-07-30 20:08:07 +0400

    adding new feature to the program

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,56 @@
 
-
+// first we call the classes and ids by using selector.
 let addform = document.querySelector("#add-book");
-let input = document.querySelector("hola");
+let input = document.querySelector(".hola");
 let addbtn = document.querySelector("button");
 let unorder = document.querySelector("ul");
 
 let delspn = document.querySelectorAll(".delete");
 let cheko = document.querySelector("#hide");
 
+
+// i need when the user add a new book then press the button. 
+// to add the new book to the bookList. 
 addbtn.addEventListener("click" , function(e){
     //step 1 -  prevent reload behaviour 
     e.preventDefault();
     //step 2 - take user value from input 
     uservalue = input.value ; 
-    //step 3  verfication as the value is not ""
+    //step 3 - verfication as the value is not ""
     if(uservalue != "" ){
-    //step 4 creating elements    
+        //step 4 - creating elements
+        // create Element (li,span) is used for creat an html elements like he li and span.     
         let lst =  document.createElement("li");
-        
         let spn = document.createElement("span");
         let del = document.createElement("span");
 
+        // add <li> and inside it create 2 span's.
         lst.appendChild(spn);
         lst.appendChild(del);
 
+        // add classes to the span named by........ .
         spn.classList.add("name");
         del.classList.add("delete");
 
-        // console.log(lst);
+        // add a text to the span.
         spn.textContent = uservalue;
         del.textContent = "delete";
 
+        // add the created li with the 2 span t un ordered list.
         unorder.appendChild(lst);
-        
-        
 
     }   
     
-    input.value = ""
-    
+    // to reset the value from the text input.
+    input.value = ""   
 
-});
+})
 
 unorder.addEventListener("click" , function(e){
     if(e.target.className == "delete"){
 
         listparent = e.target.parentElement;
 
-        // console.log(listparent);
         unorder.removeChild(listparent)
 
     }
